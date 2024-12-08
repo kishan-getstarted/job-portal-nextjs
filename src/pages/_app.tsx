c7f92fc7b09f5fbc273ff2e33afc94eef4bd9241
Kishan Oza <kishan@sayitnow.ai>
2024-12-08 17:02:26 +0530

    added prisma, material mui, trpc

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,49 @@
 import "@/styles/globals.css";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton
+} from '@clerk/nextjs'
 import type { AppProps } from "next/app";
+import { AppCacheProvider } from '@mui/material-nextjs/v15-pagesRouter';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { Roboto } from 'next/font/google';
 
+const roboto = Roboto({
+  weight: ['300', '400', '500', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-roboto',
+});
+
+const theme = createTheme({
+  typography: {
+    fontFamily: roboto.style.fontFamily,
+  },
+});
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <AppCacheProvider>
+      <ThemeProvider theme={theme}>
+        <main className={roboto.variable}>
+          <ClerkProvider>
+            <SignedOut>
+              <SignInButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+              
+            </SignedIn>
+            <Component {...pageProps} />
+          </ClerkProvider>
+        </main>
+      </ThemeProvider>
+
+
+
+    </AppCacheProvider >
+
+  )
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,20 @@
-import { Html, Head, Main, NextScript } from "next/document";
-
-export default function Document() {
+import { Html, Head, Main, NextScript, DocumentProps, DocumentContext } from "next/document";
+import { documentGetInitialProps, DocumentHeadTags, type DocumentHeadTagsProps } from '@mui/material-nextjs/v15-pagesRouter';
+export default function Document(props: DocumentProps & DocumentHeadTagsProps) {
   return (
     <Html lang="en">
-      <Head />
+      <Head >
+      <DocumentHeadTags {...props} />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
   );
+  
 }
+Document.getInitialProps = async (ctx: DocumentContext) => {
+  const finalProps = await documentGetInitialProps(ctx);
+  return finalProps;
+};
